d3077724e2ab9bfbfed320936f6ab8b48c2978d4
15105956383 <1481436573@qq.com>
2024-05-10 10:49:18 +0800

    6666
    6666

---
+++ main.c
@@ -0,0 +1,18 @@
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+#include<stdio.h>
+#include<math.h>
+int main(){
+	int a,c,i,e = 12;
+	float b,sum = 0;
+	scanf("%d,%f,%d",&a,&b,&c);
+	double result;
+	result =(double) b / e;
+	for(i=1;i<=c;i++){
+		sum=(sum+a)*(1+result);
+	}
+	printf("%.2f",sum);
+}
+ 
+
